627ddf48fd03fbb283a626f9fe39fd0b0b165972
Edwin Torres <codesandtags@gmail.com>
2025-07-19 04:45:03 +0000

    feat: added hooks

---
+++ src/hooks/usePageLeave.ts
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+React.useEffectEvent = React.experimental_useEffectEvent;
+
+export default function usePageLeave(cb) {
+  const onLeave = React.useEffectEvent((event) => {
+    const to = event.relatedTarget || event.toElement;
+    if (!to || to.nodeName === "HTML") {
+      cb();i
+    }
+  });
+
+  React.useEffect(() => {
+    document.addEventListener("mouseout", onLeave);
+
+    return () => {
+      document.removeEventListener("mouseout", onLeave);
+    };
+  }, []);
+}
